ff58832a1f9d887b11e04be67ce10a971818c574
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-12-25 14:02:17 +0530

    Advance Java | Day-1

---
+++ App2/src/HelloWorldServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out =  response.getWriter();
+        out.println("Hello World");
+
+        out.close();
+    }
+}
