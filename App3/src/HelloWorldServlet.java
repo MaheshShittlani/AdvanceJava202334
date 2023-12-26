a123907cd0c995b0f1b60a9af7a218a690db6f74
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-12-26 09:02:04 +0530

    Day-2 | Annotation based servlet config

---
+++ App3/src/HelloWorldServlet.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out =  response.getWriter();
+
+        out.println("<!doctype html>");
+        out.println("<html>");
+            out.println("<head><title>Annotation based web app</title></head>");
+            out.println("<body>");
+                out.println("Hello World");
+            out.println("</body>");
+        out.println("</html>");
+        
+
+        out.close();
+    }
+}
